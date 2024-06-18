e494f2b30edaf2839711f809c28b5154007fc37e
Momo12 <167149175+mjoklfdsdfjl@users.noreply.github.com>
2024-06-18 11:20:35 +0900

    Delete resources directory

--- resources/static/js/headfoot.js
+++
@@ -1,6 +0,0 @@
-$("nav > ul > li").mouseenter(function(){
-    $(this).children(".submenu").stop().slideDown(400);
-  });
-  $("nav > ul > li").mouseleave(function(){
-    $(this).children(".submenu").stop().slideUp(100);
-  });
